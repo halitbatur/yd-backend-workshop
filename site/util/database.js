b0e471dd42590930ea0cd2e608950adeb4cd8323
halitbatur <halitfuatbatur@gmail.com>
2021-07-29 14:35:50 +0300

    connect to db

--- site/util/database.js
+++ site/util/database.js
@@ -8,3 +8,21 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool.promise();
+const { Sequelize } = require('sequelize');
+
+// Option 2: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('yd-backend-workshop', 'halit', '24118572', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  } 
+})()
+
+
